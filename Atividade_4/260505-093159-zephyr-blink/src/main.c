6b00d1ab4ec96ffde08404793c6420e64f230200
printRafaelprog <rafael.gilfelso@gmail.com>
2026-05-26 11:49:51 -0300

    Comecando atividade 4

--- Atividade_4/260505-093159-zephyr-blink/src/main.c
+++ Atividade_4/260505-093159-zephyr-blink/src/main.c
@@ -1,33 +1,14 @@
-/* Define o endereço do registrador SIM_SCGC5 */
-#define SIM_SCGC5 (*((volatile unsigned int*)0x40048038))
-#define GPIOB_PDDR (*((volatile unsigned int*)0x400FF054))
-#define GPIOB_PSOR (*((volatile unsigned int*)0x400FF044))
-#define GPIOB_PDOR (*((volatile unsigned int*)0x400FF040))
-#define PORTB_PCR19 (*((volatile unsigned int*)0x4004A04C))
+#include "MKL25Z4.h"
 
-
-void delayMs (int n) {
-	volatile int i;
-	volatile int j;
-	for (i = 0; i < n; i++)
-		for (j = 0; j < 21000; j++) {}
-}
 
 int main(void)
 {   
+    SIM->SCGC5 |= (1<<10);
+    PORTB->PCR[19] |= (1<<8);
+    SIM->SCGC6 |= (1<<27);
+    ADC0->SC2 &= 0xFFFFFFBF;
     
-    SIM_SCGC5 |= (1<<10);
-    PORTB_PCR19 |= (1<<8);
-    GPIOB_PDDR |= (1<<19);
-    GPIOB_PDOR |= (0<<19);
-    while (1){
-        delayMs(500);
-        GPIOB_PDOR |= (1<<19);
-        delayMs(500);
-        GPIOB_PDOR &= 0xFFF7FFFF;
-    }
-
-
+    
 
     return 0;
 }
