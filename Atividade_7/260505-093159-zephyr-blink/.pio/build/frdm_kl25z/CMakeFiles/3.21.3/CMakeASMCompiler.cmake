3afa76b226cbd1f0391a6d1074b05c8080c87111
printRafaelprog <rafael.gilfelso@gmail.com>
2026-06-16 08:32:53 -0300

    branch Atividade_8

---
+++ Atividade_7/260505-093159-zephyr-blink/.pio/build/frdm_kl25z/CMakeFiles/3.21.3/CMakeASMCompiler.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_ASM_COMPILER "C:/Users/Aluno/.platformio/packages/toolchain-gccarmnoneeabi@1.80201.181220/bin/arm-none-eabi-gcc.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "C:/Users/Aluno/.platformio/packages/toolchain-gccarmnoneeabi@1.80201.181220/bin/arm-none-eabi-ar.exe")
+set(CMAKE_ASM_COMPILER_AR "C:/Users/Aluno/.platformio/packages/toolchain-gccarmnoneeabi@1.80201.181220/bin/arm-none-eabi-gcc-ar.exe")
+set(CMAKE_RANLIB "C:/Users/Aluno/.platformio/packages/toolchain-gccarmnoneeabi@1.80201.181220/bin/arm-none-eabi-ranlib.exe")
+set(CMAKE_ASM_COMPILER_RANLIB "C:/Users/Aluno/.platformio/packages/toolchain-gccarmnoneeabi@1.80201.181220/bin/arm-none-eabi-gcc-ranlib.exe")
+set(CMAKE_LINKER "c:/users/aluno/.platformio/packages/toolchain-gccarmnoneeabi@1.80201.181220/arm-none-eabi/bin/ld.bfd.exe")
+set(CMAKE_MT "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
